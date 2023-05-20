e8a21c547569032baac4291a89594a3d042fc8a0
Sara Sapundzija <sara.sapundzija@gmail.com>
2023-05-21 00:33:29 +0200

    slucajno

--- demo/src/main/java/com/example/demo/entity/Zanr.java
+++ demo/src/main/java/com/example/demo/entity/Zanr.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Entity
 public class Zanr implements Serializable {
     @Id
@@ -28,15 +27,17 @@ public class Zanr implements Serializable {
     public void setNaziv(String naziv) {
         this.naziv = naziv;
     }
-    /*@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private Knjiga knjiga;*/
+    /*
+     * @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+     * private Knjiga knjiga;
+     */
 
     @Override
     public String toString() {
         return "Zanr{" +
                 "Id=" + Id +
                 ", naziv='" + naziv + '\'' +
-                //", knjiga=" + knjiga +
+                // ", knjiga=" + knjiga +
                 '}';
     }
 }
