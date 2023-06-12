feec1d45838781c850e98b4f42ee131ae6971b63
Sara Sapundzija <sara.sapundzija@gmail.com>
2023-06-12 12:51:01 +0200

    korisnik service ispravka

--- demo/src/main/java/com/example/demo/service/KorisnikService.java
+++ demo/src/main/java/com/example/demo/service/KorisnikService.java
@@ -68,9 +68,9 @@ public class KorisnikService {
     public Korisnik registruj(String ime, String prezime, String korisnickoIme, String mejl, String lozinka){
         Korisnik korisnik = korisnikRepository.getByKorisnickoIme(korisnickoIme);
         if(korisnik == null){  //ako ne postoji korisnik sa tim korisnickim imenom, uspesno se registruje
-            policaService.save("Read", korisnik.getId());
-            policaService.save("Currently Reading", korisnik.getId());
-            policaService.save("Want to Read", korisnik.getId());
+            policaService.save("Read", korisnik);
+            policaService.save("Currently Reading", korisnik);
+            policaService.save("Want to Read", korisnik);
             return korisnikRepository.save(korisnik);
         }
         else
