2e00fee4384c9db5ab8fe16ec0d431f4cfc6f719
Ranka Radulovic <radulovic.in16.2021@uns.ac.rs>
2023-06-22 22:24:54 +0200

    lista korisnika i zanrova

---
+++ frontend/src/views/ListaKorisnika.vue
@@ -0,0 +1,58 @@
+<template>
+    <h1>Korisnici</h1>
+    <br>
+        <div class = "korisnik1">
+            <table id = "korisnici">
+                <tr>
+                    <th>Id</th>
+                    <th>Ime</th>
+                    <th>Prezime</th>
+                    <th>Korisnicko ime</th>
+                    <th>Mejl adresa</th>
+                </tr>
+                
+                <ZanrComp v-for = "korisnik in korisnici"
+                    :key = "korisnik.korisnikId"
+                    :korisnikId = "korisnik.korisnikId"
+                    :ime = korisnik.ime
+                    :prezime = korisnik.prezime
+                    :korisnickoIme = korisnik.korisnickoIme
+                    :mejlAdresa = korisnik.mejlAdresa>
+                </ZanrComp>
+            </table>
+        </div>
+</template>
+
+<script>
+import KorisnikComp from '@/components/KorisnikComponent.vue'
+export default {
+    name: "ListaKorisnika",
+    data: function(){
+        return{
+            korisnici: [],
+        };
+    },
+    components:{
+        KorisnikComp,
+    },
+    mounted(){
+        fetch('http://localhost:9090/api/korisnici')
+            .then(response => response.json())
+            .then(data => {
+                console.log("Success:", data);
+                this.korisnici = data;
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+    }
+}
+</script>
+<style>
+    h1{
+        color:coral;
+    }
+    .zanr1{
+        display: flex;
+    }
+</style>
