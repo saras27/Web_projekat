d7f8ce76cbbbbb38fdaee19e0446308ef0f4c5d8
Ranka Radulovic <radulovic.in16.2021@uns.ac.rs>
2023-06-23 18:12:20 +0200

    konflikti izmenjeno 1

--- frontend/src/components/ZanrComponent.vue
+++ frontend/src/components/ZanrComponent.vue
@@ -1,11 +1,7 @@
 <template>
     <tr>
         <td>{{ zanrId }}</td>
-<<<<<<< HEAD
-        <td>{{ naziv }}</td>
-=======
         <td>{{ Naziv }}</td>
->>>>>>> 81556e65bcb24d565c4eee999602c79150040455
     </tr>
 </template>
 
